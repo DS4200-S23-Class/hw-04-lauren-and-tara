5dcbb3ac88e180d80a4fae6c06292a12c386639f
tarajoshii <119441296+tarajoshii@users.noreply.github.com>
2023-02-14 14:53:34 -0500

    UFHHH

--- mainhw4.js
+++ mainhw4.js
@@ -8,11 +8,20 @@ function getPoint(d){
     return x
 }
 
+function plotPoint(x,y) {
+  x_position = (50*(x-1)) + 100;
+  y_position = 350-(40*(y-1));
+  x_pos = x_position.toString();
+  y_pos = y_position.toString();
+  new_point = (x_pos, y_pos);
+  return new_point;
+  console_log(new_point);
 
- //function getPoint(d){
-   // point = d.getAttribute("data-value");
-  //  var paragraph = document.getElementById("p");
+  r
+}
+
   //  var text = document.createTextNode("point");
 
    // paragraph.appendChild(text)
+
 
